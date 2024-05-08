bb02aee6d1565b8d6e2afb578377199b39d3f19d
Merve Ağaçayak <mrve.agcyk@gmail.com>
2024-05-08 23:30:26 +0300

    layout changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Eklediğimiz kütüphane
 import 'package:flyingbird/game/flyingbird_game.dart';
 import 'package:flyingbird/screens/game_over.dart';
 import 'package:flyingbird/screens/main_menu.dart';
@@ -7,7 +8,14 @@ import 'package:flyingbird/screens/main_menu.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final game = FlappyBirdGame();
+
+  // Status bar rengini transparent yapmak için
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false, // Debug yazısını kaldırmak için
     home: GameWidget(
       game: game,
       initialActiveOverlays: const ['mainMenu'],
